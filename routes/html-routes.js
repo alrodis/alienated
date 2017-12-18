a420b69f10218b08de53fdb3110a8d98e7fba5bb
Alvin Rodis <alrodis147@gmail.com>
2017-12-17 18:49:32 -0600

    12-17 commit with comments in code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,34 +6,36 @@ var path = require("path");
 // =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+    // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/homepage.html"));
-  });
+    // route loads the "Landing/Home" page
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/homepage.html"));
+    });
 
-  app.get("/report", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/report.html"));
-  });
+    // route loads the "Report UFO Sighting" page 
+    app.get("/report", function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/report.html"));
+    });
 
-  app.get("/comments", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/comments.html"));
-  });
+    // route loads the "Likelihood of Abduction" page
+    app.get("/abduction", function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/abduction.html"));
+    });
 
-  app.get("/abduction", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/abduction.html"));
-  });
+    // route loads the "Search UFO Sightings" page
+    app.get("/comments", function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/comments.html"));
+    });
 
-  app.get("/map", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/d3Test.html"));
-  });
-  
-  app.get("/marsRoverImages", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/marsRoverImages.html"));
-  });
-  // app.get("/comments", function(req, res) {
-  //   res.sendFile(path.join(__dirname + "../views/homepage.html"));
-  //  });
+    // route loads the "Mars Rover photos" page
+    app.get("/marsRoverImages", function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/marsRoverImages.html"));
+    });
+
+    // // testing for D3 mapping, this page is not in use
+    //   app.get("/map", function(req, res) {
+    //   res.sendFile(path.join(__dirname, "../views/d3Test.html"));
+    //   });
 
 };
--- views/public/assets/js/comments.js
+++ views/public/assets/js/comments.js
@@ -1,45 +1,35 @@
-
+//Post Route On-Click event when user searches for UFO sightings. 
 $("#submit").on("click", runSightingQuery);
 
+// function to retrieve data and display it dynamically on page
+function runSightingQuery(event) {
+    event.preventDefault();
+    $("#searchResults").empty();
+    //grabbing the selected state from search box on form
+    var userState = $(".custom-select").val().trim().toLowerCase();
+    var currentURL = window.location.orgin;
 
-function runSightingQuery(event){
-	event.preventDefault();
-	$("#searchResults").empty();
-	// var userState =  $("#state").val().trim().toLowerCase();
-	var userState =  $(".custom-select").val().trim().toLowerCase();
-	var currentURL =  window.location.orgin;
+    $.ajax({
+        url: "/comments",
+        method: "POST",
+        data: {
+            userState: userState
+        }
+    }).done(function(data) {
 
-	$.ajax({url: "/comments", 
-		method: "POST",
-		data: {
-			userState: userState
-		}
-		}).done(function(data){
+    //looping through database to grab the matching state user selected
+    //dynamically updating page with that data
+        for (var i = 0; i < data.length; i++) {
 
-			
-			for (var i = 0; i < data.length; i++) {
-
-				var resultDiv = $("<div>");
-				resultDiv.addClass("circle");
-				resultDiv.attr("id", "commentCircle","col-md-3", i + 1);
-				$("#searchResults").append(resultDiv);
-
-				var commentText = $("<p>");
-				commentText.attr("id", "commenttext");
-				commentText.html(data[i].comments + "<br/>" + "<br/>" + data[i].city + ", " + data[i].state);
-
-				
-				resultDiv.append(commentText)
-				
-			}
-
-
-
-			
-
-			console.log(data);
-
-
-		
-		})
+            var resultDiv = $("<div>");
+            resultDiv.addClass("circle");
+            resultDiv.attr("id", "commentCircle", "col-md-3", i + 1);
+            $("#searchResults").append(resultDiv);
+            var commentText = $("<p>");
+            commentText.attr("id", "commenttext");
+            commentText.html(data[i].comments + "<br/>" + "<br/>" + data[i].city + ", " + data[i].state);
+            resultDiv.append(commentText);
+        }
+        console.log(data);
+    });
 }
